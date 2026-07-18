3051d59019bb3c656ee91af2cab7d992b936bbfd
Adrixton2 <deathson18@gmail.com>
2026-07-18 23:40:15 +0200

    deploy: publicar version de produccion

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// Copia este archivo como firebase-config.js para desarrollo local.
+// No subas firebase-config.js al repositorio.
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "PEGAR_API_KEY",
+  authDomain: "PEGAR_AUTH_DOMAIN",
+  projectId: "PEGAR_PROJECT_ID",
+  storageBucket: "PEGAR_STORAGE_BUCKET",
+  messagingSenderId: "PEGAR_MESSAGING_SENDER_ID",
+  appId: "PEGAR_APP_ID"
+};
